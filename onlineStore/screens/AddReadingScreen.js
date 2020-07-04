9b7ed126e925f07215010054060bbd9babbb6443
DanielaTobar <nadi_toci@outlook.com>
2020-07-04 17:28:25 -0500

    empezar con historial

---
+++ onlineStore/screens/AddReadingScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { SafeAreaView  } from 'react-native';
+import Layout from '../constants/Layout';
+
+export default function AccountScreen () {
+  return (
+    <SafeAreaView style={Layout.safeArea}>
+    </SafeAreaView>
+  );
+} 
