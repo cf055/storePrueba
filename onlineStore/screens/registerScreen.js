649ba22a959b163f89f0ed21ef802b90c7a5900f
DanielaTobar <nadi_toci@outlook.com>
2020-07-04 17:24:40 -0500

    cache borrado

--- onlineStore/screens/registerScreen.js
+++ onlineStore/screens/registerScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, ImageBackground, TextInput, Image, SafeAreaView , TouchableOpacity  } from 'react-native';
+import { View, Text, TextInput, Image, SafeAreaView , TouchableOpacity  } from 'react-native';
 import Layout from '../constants/Layout'
 
 export default function RegisterScreen({ navigation }) {
@@ -9,12 +9,12 @@ export default function RegisterScreen({ navigation }) {
   <SafeAreaView style={Layout.safeArea}>
     <View style={Layout.containerDesing}>
       <Image style={{marginTop:10, borderRadius:200, width:160, height:160}} source={require('../assets/images/Logo.png')} />
-      <Text style={{fontSize: 25, textAlign: 'center', fontWeight: "bold",}}>Registro app.</Text>
+      <Text style={{fontSize: 25, textAlign: 'center', fontWeight: "bold",}}>Registro app</Text>
       <TextInput style={Layout.inputDesing} placeholder={'Name'} />
       <TextInput style={Layout.inputDesing} placeholder={'Correo'} />
       <TextInput style={Layout.inputDesing} placeholder={'Contraseña'} />
       <TouchableOpacity  onPress={() => navigation.navigate('Login')}  style={Layout.buttonDesing}>
-        <Text style={Layout.textButtonDesing} >Registrarse.</Text>
+        <Text style={Layout.textButtonDesing} >Registrarse</Text>
       </TouchableOpacity>
     </View>
   </SafeAreaView>
