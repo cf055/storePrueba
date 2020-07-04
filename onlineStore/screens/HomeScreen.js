1ba1c5ac960fee23a3cb16234b2ff83cf10979b1
DanielaTobar <nadi_toci@outlook.com>
2020-07-04 16:14:52 -0500

    home cambiado el container

--- onlineStore/screens/HomeScreen.js
+++ onlineStore/screens/HomeScreen.js
@@ -5,7 +5,7 @@ import Layout from '../constants/Layout'
 export default function HomeScreen() {
   return(
     <SafeAreaView style={Layout.safeArea}>
-      <View style={Layout.containerHomeScreen}>
+      <View style={Layout.containerDesing}>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <TouchableOpacity  onPress={() => navigation.navigate('Login')}  style={Layout.buttonDesing}>
             <Text style={{color: "white", fontSize: 15, fontWeight: "bold"}} >Historial.</Text>
