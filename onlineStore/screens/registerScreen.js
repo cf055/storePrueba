8571bbce0c903cc30bb3ff0983532ecd3a408169
cf055 <ricardof.grb@hotmail.com>
2020-07-04 16:55:25 -0500

    arreglarScreens

--- onlineStore/screens/registerScreen.js
+++ onlineStore/screens/registerScreen.js
@@ -8,24 +8,14 @@ export default function RegisterScreen({ navigation }) {
 
   <SafeAreaView style={Layout.safeArea}>
     <View style={Layout.containerDesing}>
-      <View style={{flex:2, justifyContent: 'center'}}>
-        <Image style={{borderRadius:150, alignItems: 'center', width:100, height:100}} source={require('../assets/images/Logo.png')} />
-      </View>
-      <View style={{flex:3}}>
+      <Image style={{marginTop:10, borderRadius:200, width:160, height:160}} source={require('../assets/images/Logo.png')} />
       <Text style={{fontSize: 25, textAlign: 'center', color:'#5664af', fontWeight: "bold",}}>Registro app.</Text>
-      
       <TextInput style={Layout.inputDesing} placeholder={'Name'} />
-      
       <TextInput style={Layout.inputDesing} placeholder={'Correo'} />
-      
       <TextInput style={Layout.inputDesing} placeholder={'Contraseña'} />
-
       <TouchableOpacity  onPress={() => navigation.navigate('Login')}  style={Layout.buttonDesing}>
         <Text style={Layout.textButtonDesing} >Registrarse.</Text>
       </TouchableOpacity>
-      </View>
-      <View style={{flex:1}}>
-      </View>
     </View>
   </SafeAreaView>
   )
