f4706fcc14875903637f9768ea8064c4114c2687
DanielaTobar <nadi_toci@outlook.com>
2020-07-04 20:12:04 -0500

    esqueleto pantalla new ascann

--- onlineStore/screens/AddReadingScreen.js
+++ onlineStore/screens/AddReadingScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, ScrollView, Image, SafeAreaView, TextInput } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, SafeAreaView, TextInput } from 'react-native';
 import Layout from '../constants/Layout';
 
 export default function AddReadingScreen () {
@@ -8,15 +8,37 @@ export default function AddReadingScreen () {
       <View style={{flex: 1}}>
         <View style={Layout.viewTitle}>
           <View style={{ justifyContent: 'center'}}>
-          <Text style={{color: "white", fontSize: 25}}>Añadir Scann</Text>
+            <Text style={{color: "white", fontSize: 25}}>Añadir Scann</Text>
+          </View>
         </View>
-        </View>
+
         <View style={{flex:3, alignItems: 'stretch'}}>
-        <ScrollView>
-        </ScrollView>
+          <ScrollView style={{backgroundColor:'white'}}>
+            <Text style={{color:"#5664af", marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Id Medidor</Text>
+            <Text style={{color:"#5664af", marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Fecha - Hora</Text>
+            <Text style={{color:"#5664af", marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Localizacion</Text>
+            <Text style={{color:"#5664af", marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Mac</Text>
+
+            <Text style={{color:"#5664af", marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Medida</Text>
+            <TextInput style={styles.input2} />
+            <Text style={{color:"#5664af", marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Comentario</Text>
+            <TextInput style={styles.input2} />
+            <Text style={{color:"#5664af", marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Observacion</Text>
+            <TextInput style={styles.input2} />
+          </ScrollView>
         </View>
-        
       </View>
     </SafeAreaView>
   );
 } 
+
+const styles = StyleSheet.create({ 
+  input2: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: "white",
+    borderBottomColor: "#a2a5a4",
+    padding: 10,
+    marginHorizontal: 30
+  }
+});
--- onlineStore/screens/HomeScreen.js
+++ onlineStore/screens/HomeScreen.js
@@ -8,10 +8,10 @@ export default function HomeScreen({ navigation }) {
       <View style={Layout.containerDesing}>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <TouchableOpacity  onPress={() => navigation.navigate('AddReading')}  style={Layout.buttonDesing}>
-            <Text style={Layout.textButtonDesing} >Historial.</Text>
+            <Text style={Layout.textButtonDesing} >Historial</Text>
           </TouchableOpacity>
           <TouchableOpacity  onPress={() => navigation.navigate('AddReading')}  style={Layout.buttonDesing}>
-            <Text style={Layout.textButtonDesing} >Scan.</Text>
+            <Text style={Layout.textButtonDesing} >Nuevo Scan</Text>
           </TouchableOpacity>
         </View>
       </View>
