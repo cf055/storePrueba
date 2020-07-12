b32d9a1d63359bf43c84a6ba378f23d9d48e6ed2
cf055 <ricardof.grb@hotmail.com>
2020-07-12 17:42:27 -0500

    pantallasArreglar2

--- onlineStore/screens/AddReadingScreen.js
+++ onlineStore/screens/AddReadingScreen.js
@@ -4,19 +4,36 @@ import Layout from '../constants/Layout';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import * as Location from 'expo-location'; // localizacion
 import TimeEjemplo from '../Services/ejemplo';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 import { dateTime }  from '../Services/DateTimeService';
 
 export default function AddReadingScreen () {  
   const [location, setLocation] = useState(null); // localizacion
   const [errorMsg, setErrorMsg] = useState(null); // localizacion
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
 
   // localizacion todo esto
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestPermissionsAsync();
+      const { status2 } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
+      }
+
+      const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+      };
+    
+      if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+      }
+      if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
       }
 
       //.timestamp para hora
@@ -68,6 +85,12 @@ export default function AddReadingScreen () {
                 style={Layout.floatButton}>
                 <MaterialCommunityIcons onPress={() => console.log('scanea aqui')} name="qrcode-scan" size={50} color="white" />
               </TouchableOpacity>
+              <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
             </View>
 
         </View>
