f327c6d1c878c5aff174dae2ae3f113e493cdbef
cf055 <ricardof.grb@hotmail.com>
2020-07-12 16:52:29 -0500

    ejemploPantallas2

---
+++ onlineStore/Services/ejemplo.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react' // localizacion
+import { View, Text, ScrollView, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+import Layout from '../constants/Layout';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export default function TimeEjemplo({value,value2}){
+    return (
+      <SafeAreaView style={Layout.safeArea}>
+        <View style={{flex: 1}}>
+          <View style={Layout.viewTitle}>
+            <View style={{ justifyContent: 'center'}}>
+              <Text style={{color: "white", fontSize: 25}}>Añadir Scann</Text>
+            </View>
+          </View>
+  
+          <View style={{flex:3, backgroundColor:'white'}}>
+            <ScrollView>
+              <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Id Medidor</Text>
+              <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Fecha - Hora</Text>
+              <Text>{value}</Text>
+              <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Localizacion</Text>
+              <Text>{value2}</Text>
+              <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Mac</Text>
+  
+              <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Medida</Text>
+              <TextInput placeholder='Ingrese medidia' style={Layout.inputWithoutBorder} />
+              <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Comentario</Text>
+              <TextInput placeholder='Ingrese comentario' style={Layout.inputWithoutBorder} />
+              <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Observacion</Text>
+              <TextInput placeholder='Ingrese observacion' style={Layout.inputWithoutBorder} />
+  
+            </ScrollView>
+  
+            <View style={{alignItems:'flex-end', bottom:30, right:30}}>
+                <TouchableOpacity
+                  style={Layout.floatButton}>
+                  <MaterialCommunityIcons onPress={() => console.log('scanea aqui')} name="qrcode-scan" size={50} color="white" />
+                </TouchableOpacity>
+              </View>
+  
+          </View>
+  
+        </View>
+      </SafeAreaView>
+    );
+  }
--- onlineStore/screens/AddReadingScreen.js
+++ onlineStore/screens/AddReadingScreen.js
@@ -4,6 +4,7 @@ import Layout from '../constants/Layout';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import * as Location from 'expo-location'; // localizacion
+import TimeEjemplo from '../Services/ejemplo';
 
 export default function AddReadingScreen () {  
   const [location, setLocation] = useState(null); // localizacion
@@ -49,44 +50,3 @@ export default function AddReadingScreen () {
   );
 } 
 
-function TimeEjemplo({value,value2}){
-  return (
-    <SafeAreaView style={Layout.safeArea}>
-      <View style={{flex: 1}}>
-        <View style={Layout.viewTitle}>
-          <View style={{ justifyContent: 'center'}}>
-            <Text style={{color: "white", fontSize: 25}}>Añadir Scann</Text>
-          </View>
-        </View>
-
-        <View style={{flex:3, backgroundColor:'white'}}>
-          <ScrollView>
-            <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Id Medidor</Text>
-            <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Fecha - Hora</Text>
-            <Text>{value}</Text>
-            <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Localizacion</Text>
-            <Text>{value2}</Text>
-            <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Mac</Text>
-
-            <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Medida</Text>
-            <TextInput placeholder='Ingrese medidia' style={Layout.inputWithoutBorder} />
-            <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Comentario</Text>
-            <TextInput placeholder='Ingrese comentario' style={Layout.inputWithoutBorder} />
-            <Text style={{marginHorizontal: 30, color: "#a2a5a4", margin: 10}}>Observacion</Text>
-            <TextInput placeholder='Ingrese observacion' style={Layout.inputWithoutBorder} />
-
-          </ScrollView>
-
-          <View style={{alignItems:'flex-end', bottom:30, right:30}}>
-              <TouchableOpacity
-                style={Layout.floatButton}>
-                <MaterialCommunityIcons onPress={() => console.log('scanea aqui')} name="qrcode-scan" size={50} color="white" />
-              </TouchableOpacity>
-            </View>
-
-        </View>
-
-      </View>
-    </SafeAreaView>
-  );
-}
