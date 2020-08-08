9a02e4878fabc78aa0708dc9126dea6c453c5e62
DanielaTobar <nadi_toci@outlook.com>
2020-08-08 18:00:31 -0500

    contexto cambado si funciona

--- onlineStore/App.js
+++ onlineStore/App.js
@@ -3,16 +3,15 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import TapsScreen from './screens/TapsScreen';
 import AddReadingScreen from './screens/AddReadingScreen';
-import * as context from './database/Context';
+import { IdUser } from './database/Context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  const userToken = context.idUser;
 
-  if (userToken != null && userToken.length != 0) {
+  if (IdUser != null && IdUser.length != 0) {
     return (  
       <NavigationContainer>
         <Stack.Navigator headerMode="none">
